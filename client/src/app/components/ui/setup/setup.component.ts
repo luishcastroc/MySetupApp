c31c7eff6f5437d791b6b960bad3943443f9575d
Luis Castro <luis.castro@agilethought.com>
2020-12-22 13:07:47 -0500

    adding loading pipe to handle statuses

--- client/src/app/components/ui/setup/setup.component.ts
+++ client/src/app/components/ui/setup/setup.component.ts
@@ -12,7 +12,5 @@ export class SetupComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit(): void {
-    console.log('setup: ', this.setup);
-  }
+  ngOnInit(): void {}
 }
--- client/src/app/components/ui/ui.module.ts
+++ client/src/app/components/ui/ui.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SetupComponent } from './setup/setup.component';
+import { WithLoadingPipe } from '../../_pipes/withloading.pipe';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [SetupComponent],
-  exports: [SetupComponent],
+  declarations: [SetupComponent, WithLoadingPipe],
+  exports: [SetupComponent, WithLoadingPipe],
 })
 export class UiModule {}
