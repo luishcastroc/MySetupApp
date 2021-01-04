d22f1352d5fc2ff82e02e4d17f58b75547b17f7d
Luis Castro <luis.castro@agilethought.com>
2021-01-03 21:42:30 -0500

    add user tabs

--- client/src/app/components/ui.module.ts
+++ client/src/app/components/ui.module.ts
@@ -2,10 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SetupComponent } from '@components/setup/setup.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { UserTabsComponent } from './user-tabs/user-tabs.component';
 
 @NgModule({
-  imports: [CommonModule, FontAwesomeModule],
-  declarations: [SetupComponent],
-  exports: [FontAwesomeModule, SetupComponent],
+  imports: [CommonModule, FontAwesomeModule, TabsModule.forRoot()],
+  declarations: [SetupComponent, UserTabsComponent],
+  exports: [FontAwesomeModule, SetupComponent, UserTabsComponent],
 })
 export class UiModule {}
---
+++ client/src/app/components/user-tabs/user-tabs.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-tabs',
+  templateUrl: './user-tabs.component.html',
+  styleUrls: ['./user-tabs.component.scss'],
+})
+export class UserTabsComponent {}
